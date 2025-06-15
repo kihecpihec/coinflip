bc0c32a6658b4495fc0882bcd72a05204a527760
kihec <hananmesic301@gmail.com>
2025-06-15 17:28:30 +0200

    Inital commit

---
+++ CoinFlip/CoinFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinFlipApp.swift
+//  CoinFlip
+//
+//  Created by kihec on 15. 6. 25.
+//
+
+import SwiftUI
+
+@main
+struct CoinFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinFlip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinFlip
+//
+//  Created by kihec on 15. 6. 25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
